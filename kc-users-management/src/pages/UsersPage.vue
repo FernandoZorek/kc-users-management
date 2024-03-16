68fddfc341776e2e37246aec17f539a68465a17a
Fernando.Zorek <fernando.zorek@icolabora.com>
2024-03-15 21:05:59 -0300

    Routes and pages files created

---
+++ kc-users-management/src/pages/UsersPage.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <q-page class="flex flex-center">
+    <h3>{{ i18n.t("usersText") }}</h3>
+  </q-page>
+</template>
+
+<script setup>
+
+import {inject} from "vue";
+const i18n = inject("i18n");
+</script>
